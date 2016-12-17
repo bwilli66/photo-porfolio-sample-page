4cf3297ae4f4a6611d741b4e9557308e58d3ab3c
Brad Williams <bradwilliamsvi@gmail.com>
2016-12-16 22:49:35 -0800

    Add weather API, background changes with weather

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   var $menu = $("#sidebar-wrapper");
 
   $(document).on("click", ".js-menu-open", function(){
@@ -10,4 +11,51 @@ $(document).ready(function(){
     $menu.removeClass("open");
     return false;
   });
+
+  var getWeather = function () {
+    $.ajax({
+      url: "http://api.wunderground.com/api/20eb543aab3a31a5/geolookup/conditions/q/91301.json",
+      dataType: "jsonp",
+      success: function (response){
+        var conditions = response.current_observation.weather;
+        loadImage(conditions);
+      }
+    });
+  };
+
+  getWeather(); //Make API call for weather conditions, possible responses: "clear", "Snow", "Cloudy"
+
+  var getTimeOfDay = function () {
+    var time = new Date();
+    var hours = time.getHours();
+    var timeOfDay;
+
+    if(hours < 12){
+      timeOfDay = "morning";
+    }
+    else if(hours >= 12 && hours < 17){
+      timeOfDay = "after";
+    }
+    else{
+      timeOfDay = "night";
+    }
+
+    return timeOfDay;
+  };
+
+  var loadImage = function (_conditions) {
+    var imageSRC = "img/weather/hero-";
+    var validConditions = ["clear", "cloudy", "rain", "snow"];
+    var timeOfDay =  getTimeOfDay();
+    var conditions = _conditions.toLowerCase();
+
+    if(validConditions.indexOf(conditions) === -1){
+      conditions = "cloudy";
+    }
+
+    var url = imageSRC + conditions + "-" + timeOfDay + ".jpg";
+
+    $('#intro').css("background-image","url(" + url + ")");
+  };
+
 });
